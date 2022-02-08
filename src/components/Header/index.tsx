41ada5de199587b521974a1a4e179ebc97becf4e
Matheus <matheus.miranda@leadsoft.inf.br>
2022-02-08 16:17:27 -0300

    Created Header component usign plop.js

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+import * as I from './interfaces';
+
+export default function Header({  }: I.HeaderProps) {
+  return (
+    <S.Container>
+      <h1>Header</h1>
+    </S.Container>
+  );
+}
